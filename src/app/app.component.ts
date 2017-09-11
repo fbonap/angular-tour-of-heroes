d6143ab22b19de2cfbc56d7bc743f9d90fdd0443
Fabio Bonaparte <fbonap1983@gmail.com>
2017-09-12 00:01:18 +0200

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -30,14 +26,7 @@ const HEROES: Hero[] = [
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-          <label>name: </label>
-          <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
+    <hero-detail [hero]="selectedHero"></hero-detail>
   `,
   styleUrls: ['app/app.component.css']
 })
@@ -45,7 +34,7 @@ export class AppComponent  {
   title = 'Tour of Heroes';
   heroes = HEROES;
   selectedHero: Hero;
-  hero: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
